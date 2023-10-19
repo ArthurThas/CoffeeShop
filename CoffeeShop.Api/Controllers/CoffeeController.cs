cb6034d1c432a2bcf2a226dac77ec3bca9df9fe5
arthur <arthur.fuego@yahoo.com>
2023-10-19 15:06:33 +0800

    Initial commit

---
+++ CoffeeShop.Api/Controllers/CoffeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeController : ControllerBase
+    {
+        [HttpGet]
+        public Task<List<string>> GetTopCoffees()
+        {
+            var coffees = new List<string>
+            {
+                "Americano",
+                "Machiato",
+                "Latte"
+            };
+
+            return Task.FromResult(coffees);
+        }
+    }
+}
